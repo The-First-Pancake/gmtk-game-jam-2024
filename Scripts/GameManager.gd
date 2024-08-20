bad0023cf27107ed3f512a1a497475693177be06
Oliver Philbrick <oliveman521@gmail.com>
2024-08-19 21:39:39 -0400

    Add everything to level menmu

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -22,9 +22,10 @@ func _ready() -> void:
 		preload("res://Levels/World1/temple_8_arrows.tscn"),
 		preload("res://Levels/World1/temple_9_arrows_on_holds.tscn"),
 		preload("res://Levels/World1/temple_10_arrow rain.tscn"),
-		preload("res://Levels/World1/temple_9_breaker_intro.tscn"),
-		preload("res://Levels/World1/temple_10_reverse_breaker.tscn"),
 		preload("res://Levels/World1/temple_11_breaker_intro.tscn"),
+		preload("res://Levels/World1/temple_12_breaker_dropper.tscn"),
+		preload("res://Levels/World1/temple_13_reverse_breaker.tscn"),
+		preload("res://Levels/World1/temple_66_hell.tscn")
 	]
 	for level in levels:
 		var level_dictionary_default : Dictionary = {
